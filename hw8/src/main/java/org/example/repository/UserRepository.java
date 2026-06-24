9622fc4f35cad174f5e87d45abca4fc5eb804f21
Роман <168233178+romangd70@users.noreply.github.com>
2026-06-24 13:43:59 +0300

    Delete hw8 directory

--- hw8/src/main/java/org/example/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-import org.example.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
